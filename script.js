d12b18d1b1d6c5c64940be155d5670169152c780
Faizan <faizirajpoot098@gmail.com>
2022-06-20 17:16:26 +0500

    second commit

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ const ham_burger = document.querySelector('.ham_burger')
 burger.addEventListener('click', () => {
     ham_burger.classList.toggle('show_burger')
 })
+
+
+
+// function printName() {
+//     console.log(12);
+//     return 'Faizan Ahmad'
+// }
+// function findName() {
+//     console.log('abc');
+//     return printName()
+// }
+// function saveMyName() {
+//     console.log('def');
+//     return findName()
+// }
+
+// console.log(saveMyName());
